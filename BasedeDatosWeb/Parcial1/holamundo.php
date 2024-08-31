23c8a06930a4652e2089d232e7e3576129c80688
PoudelCeti <159517113+PoudelCeti@users.noreply.github.com>
2024-08-30 18:35:38 -0700

    Prueba

---
+++ BasedeDatosWeb/Parcial1/holamundo.php
@@ -0,0 +1,14 @@
+<?php
+
+    $nombre = "Pou";
+    $numero = 7;
+    $numero2 = "7";
+
+    $total = $numero + $numero2;
+
+    echo "Total = " . $total;
+
+    echo "<h1> HOLA MUNDO </h1>";
+
+    
+?>
